5bff05b28abb46621d26ce432f137dc49867b5ee
Abdullah <abdullahsoomro.official@gmail.com>
2023-12-22 22:43:43 +0500

    carousel updated with arrow navigation

--- index.js
+++ index.js
@@ -38,25 +38,48 @@ const previousBtn = document.querySelector(".previous-btn");
 const nextBtn = document.querySelector(".next-btn");
 let counter = 0;
 
-carouselItems.forEach((carouselItem, index) => {
-  carouselItem.style.left = `${index * 100}%`;
-});
-
 const slideCarouselItem = () => {
   carouselItems.forEach((carouselItem) => {
     carouselItem.style.transform = `translateX(-${counter * 100}%)`;
   });
 };
 
+const hideAndShowNextBtn = () => {
+  if (counter === carouselItems.length - 1) {
+    nextBtn.style.visibility = "hidden";
+  } else {
+    nextBtn.style.visibility = "visible";
+  }
+};
+
+const hideAndShowPreviousBtn = () => {
+  if (counter === 0) {
+    previousBtn.style.visibility = "hidden";
+  } else {
+    previousBtn.style.visibility = "visible";
+  }
+};
+
 const showNextCarouselItem = () => {
   if (!(counter + 1 > carouselItems.length - 1)) {
     counter++;
     slideCarouselItem();
+    hideAndShowNextBtn();
+    hideAndShowPreviousBtn();
   }
 };
+
 const showPreviousCarouselItem = () => {
   if (!(counter - 1 < 0)) {
     counter--;
     slideCarouselItem();
+    hideAndShowPreviousBtn();
+    hideAndShowNextBtn();
   }
 };
+
+// hiding previous button on page load
+hideAndShowPreviousBtn();
+carouselItems.forEach((carouselItem, index) => {
+  carouselItem.style.left = `${index * 100}%`;
+});
